9476c7ee2827b8567213c9ddbcf6e8881c2b16b4
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-17 18:49:49 +0100

    get photo by query

--- src/refs.js
+++
@@ -1,3 +0,0 @@
-export const searchBtn = document.querySelector('#searchBtn');
-export const galleryListEl = document.querySelector('ul.gallery');
-export const formEl = document.querySelector('.search-form')
